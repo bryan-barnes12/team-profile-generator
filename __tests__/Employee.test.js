bcac12668ecce00bddf9d0083b3cee0255863792
Bryan Barnes <bryan.barnes12@gmail.com>
2021-05-17 11:29:01 -0500

    Moving testing to devDep.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,25 +1,31 @@
 const Employee = require('../lib/Employee')
 
-const testEmployee = new Employee('test', 1, 'a@b.c')
 
-test('generates employee object', () => {
-    expect(testEmployee).toEqual({
-        _empName: 'test',
-        _empId: 1,
-        _empEmail: 'a@b.c',
-        _empRole: 'Employee'
+describe('Employee Class initialization', () => {
+    test('should generate a new employee object', () => {
+        const testEmployee = new Employee('test', 1, 'a@b.c')
+        expect(testEmployee).toEqual({
+            _empName: 'test',
+            _empId: 1,
+            _empEmail: 'a@b.c',
+            _empRole: 'Employee'
+        });
     });
-});
 
-test('returns employee name', () => {
-    expect(testEmployee.empName).toEqual('test');
-});
-test('returns employee id', () => {
-    expect(testEmployee.empId).toEqual(1);
-});
-test('returns employee email', () => {
-    expect(testEmployee.empEmail).toEqual('a@b.c');
-});
-test('returns employee role', () => {
-    expect(testEmployee.empRole).toEqual('Employee');
+    test('returns employee name', () => {
+        const testEmployee = new Employee('test', 1, 'a@b.c')
+        expect(testEmployee.empName).toEqual('test');
+    });
+    test('returns employee id', () => {
+        const testEmployee = new Employee('test', 1, 'a@b.c')
+        expect(testEmployee.empId).toEqual(1);
+    });
+    test('returns employee email', () => {
+        const testEmployee = new Employee('test', 1, 'a@b.c')
+        expect(testEmployee.empEmail).toEqual('a@b.c');
+    });
+    test('returns employee role', () => {
+        const testEmployee = new Employee('test', 1, 'a@b.c')
+        expect(testEmployee.empRole).toEqual('Employee');
+    });
 });
